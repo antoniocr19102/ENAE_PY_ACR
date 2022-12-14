bca4dd41d224ce8af7165cbbfd087aae2297550b
antoniocr19102 <101328203+antoniocr19102@users.noreply.github.com>
2022-12-14 12:26:56 +0100

    Update modulo2.py
    Modificar primer plot

--- modulo2.py
+++ modulo2.py
@@ -13,10 +13,7 @@ df = pd.read_csv(archivo, encoding='UTF-8')
 df.fillna('', inplace=True)
 df['Text'] = df['Text'].apply(lambda x: x.strip())
 df_sheldon_penny = df[(df.Speaker == 'Sheldon') & (df.Text == 'Penny.')]
-freq = df_sheldon_penny.groupby('Location').count()
-x = freq.index
-y = freq['Text']
-fig, ax = plt.subplots()
+x = df_sheldon_penny['Location']
 
-ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+plt.hist(x, bins=len(x), orientation='horizontal')
 plt.show()
